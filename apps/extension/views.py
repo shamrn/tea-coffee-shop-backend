671fe5d01a9a314d2a4f8de389700840671b3d00
Vladislav Shamrin <shamrin2007@mail.ru>
2022-10-01 16:04:44 +0300

    Added 'serializers' extension include 'EmptySerializer' and in views extension added mapper for view

--- apps/extension/views.py
+++ apps/extension/views.py
@@ -1,7 +1,57 @@
+from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import GenericViewSet
+
+from extension.serializers import EmptySerializer
 
 
 class AllowAnyMixin:
     """Permission classes mixin"""
 
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
+
+
+class SerializerClassMapperViewSetMixin(GenericViewSet):
+    """ViewSet serializer class mapper mixin."""
+
+    serializer_class_mapper = dict()
+
+    def get_serializer_class(self):
+        serializer_class = EmptySerializer
+
+        try:
+            action_w_method = f'{self.action}_{self.request.method}'.lower()
+
+            if action_w_method in self.serializer_class_mapper:
+                serializer_class = self.serializer_class_mapper[action_w_method]
+            elif self.action in self.serializer_class_mapper:
+                serializer_class = self.serializer_class_mapper[self.action]
+            else:
+                serializer_class = super(
+                    SerializerClassMapperViewSetMixin, self).get_serializer_class()
+        except AssertionError:
+            pass
+        finally:
+            return serializer_class
+
+
+class SerializerClassMapperAPIViewMixin(GenericAPIView):
+    """ViewSet serializer class mapper mixin."""
+
+    serializer_class_mapper = dict()
+
+    def get_serializer_class(self):
+        serializer_class = EmptySerializer
+
+        try:
+            http_method = self.request.method.lower()
+
+            if http_method in self.serializer_class_mapper:
+                serializer_class = self.serializer_class_mapper[http_method]
+            else:
+                serializer_class = super(
+                    SerializerClassMapperAPIViewMixin, self).get_serializer_class()
+        except AssertionError:
+            pass
+        finally:
+            return serializer_class
