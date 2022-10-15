9eab63d087b834d7ca3d6809979635c1bdcd0b53
Vladislav Shamrin <shamrin2007@mail.ru>
2022-10-15 19:12:57 +0300

    In development settings updated 'ALLOWED_HOSTS', added DEBUG constant

--- project/settings/development.py
+++ project/settings/development.py
@@ -1,13 +1,5 @@
 from .base import *  # NOQA
 
-ALLOWED_HOSTS = ['vladislav111.pythonanywhere.com']
+DEBUG = True
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-# --------------------------------------------------------------------------------------------------
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+ALLOWED_HOSTS = ['92.255.111.67']
